e4b0d0c2cb11d38718e1af65c12cba844668b2a4
Caio Alves <caioanegocios@gmail.com>
2024-02-13 20:24:25 -0300

    Initial config

---
+++ src/main/java/com/vagabackend/estudobackend/EstudobackendApplication.java
@@ -0,0 +1,13 @@
+package com.vagabackend.estudobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vagabackend/estudobackend/EstudobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagabackend.estudobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
